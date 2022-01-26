79e124f5375186d14288366b41b372990a43a94e
khohill <y.tsymbalistov-iat@lll.kpi.ua>
2022-01-27 00:29:29 +0200

    new proj

---
+++ src/main/java/net/mcreator/weedmod/procedures/DriedCannabisLeafFoodEatenProcedure.java
@@ -0,0 +1,10 @@
+package net.mcreator.weedmod.procedures;
+
+import java.util.Map;
+
+public class DriedCannabisLeafFoodEatenProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+
+	}
+}
