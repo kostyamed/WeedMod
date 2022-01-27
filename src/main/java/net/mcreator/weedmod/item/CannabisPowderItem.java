6b4be118feb6556622c93286aceae3b9cc0d6bc9
kostyamed <kostyamed0003@gmail.com>
2022-01-27 19:23:29 +0200

    added cannabis powder

---
+++ src/main/java/net/mcreator/weedmod/item/CannabisPowderItem.java
@@ -0,0 +1,49 @@
+
+package net.mcreator.weedmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.weedmod.WeedmodModElements;
+
+@WeedmodModElements.ModElement.Tag
+public class CannabisPowderItem extends WeedmodModElements.ModElement {
+	@ObjectHolder("weedmod:cannabis_powder")
+	public static final Item block = null;
+
+	public CannabisPowderItem(WeedmodModElements instance) {
+		super(instance, 10);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("cannabis_powder");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
