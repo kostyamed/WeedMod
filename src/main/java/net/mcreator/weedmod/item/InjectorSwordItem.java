4ffe84b2ff2f8ee81d51e25c135420e8e81cffe9
khohill <y.tsymbalistov-iat@lll.kpi.ua>
2022-01-27 14:44:13 +0200

    crafts, sword, dust

---
+++ src/main/java/net/mcreator/weedmod/item/InjectorSwordItem.java
@@ -0,0 +1,52 @@
+
+package net.mcreator.weedmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.weedmod.WeedmodModElements;
+
+@WeedmodModElements.ModElement.Tag
+public class InjectorSwordItem extends WeedmodModElements.ModElement {
+	@ObjectHolder("weedmod:injector_sword")
+	public static final Item block = null;
+
+	public InjectorSwordItem(WeedmodModElements instance) {
+		super(instance, 7);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 1;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return -1f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 0;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 3, -3f, new Item.Properties().group(ItemGroup.COMBAT)) {
+		}.setRegistryName("injector_sword"));
+	}
+}
---
+++ src/main/java/net/mcreator/weedmod/procedures/MagicDustWhenEatenProcedure.java
@@ -0,0 +1,36 @@
+package net.mcreator.weedmod.procedures;
+
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.weedmod.WeedmodMod;
+
+import java.util.Map;
+
+public class MagicDustWhenEatenProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				WeedmodMod.LOGGER.warn("Failed to load dependency entity for procedure MagicDustWhenEaten!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.WITHER, (int) 100, (int) 1));
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.HUNGER, (int) 160, (int) 1));
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.NAUSEA, (int) 160, (int) 1));
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.BLINDNESS, (int) 160, (int) 1));
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.WEAKNESS, (int) 600, (int) 1));
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SLOWNESS, (int) 600, (int) 1));
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.MINING_FATIGUE, (int) 600, (int) 1));
+	}
+}
